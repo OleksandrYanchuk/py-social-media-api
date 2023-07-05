36694a79426b415c292790b6e0bceb88564f07bd
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-07-05 16:31:34 +0300

    add scripts.py for automatic install requirements, migrate and runserver

---
+++ scripts.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Install requirements
+subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
+
+# Perform migrations
+subprocess.run(['python', 'manage.py', 'makemigrations'], check=True)
+subprocess.run(['python', 'manage.py', 'migrate', 'user'], check=True)
+subprocess.run(['python', 'manage.py', 'migrate', 'social_media_service'], check=True)
+subprocess.run(['python', 'manage.py', 'migrate'], check=True)
+subprocess.run(['python', 'manage.py', 'runserver'], check=True)
